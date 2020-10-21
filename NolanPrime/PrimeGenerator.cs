8bda7fb8784a39a317012741e93c22087f821fdc
John Nolan <jjnolan@virginmedia.com>
2020-10-21 21:16:59 +0100

    new: initial commit
    contains some code

---
+++ NolanPrime/PrimeGenerator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace NolanPrime
+{
+    public class PrimeGenerator
+    {
+        public List<int> Generate(int n)
+        {
+            List<int> primes = new List<int>(1000000);
+            primes.Add(2);      // 1 is not a prime
+            int nextPrime = 3;  // 3 is the next possible prime
+
+            while (primes.Count < n)
+            {
+                int sqrt = (int)Math.Sqrt(nextPrime);
+                
+                bool isPrime = true;
+
+                for (int i = 0; primes[i] <= sqrt; i++)
+                {
+                    if (nextPrime % primes[i] == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                
+                if (isPrime)
+                {
+                    primes.Add(nextPrime);
+                }
+                
+                nextPrime += 2;
+            }
+            
+            return primes;
+        }
+    }
+}
---
+++ NolanPrime/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NolanPrime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool outputPrimesUnordered = false; // Set this to true if you would like to output every prime (unordered)
+            bool outputPrimesOrdered = false;   // Set this to true if you would like to output every prime (ordered)
+
+            Stopwatch timer = new Stopwatch();
+         
+            Console.WriteLine(string.Format("{0}:\t\tStarting prime generation...", timer.Elapsed.ToString()));
+
+            var n = 1000000;
+
+            timer.Start();
+
+            var primes = new PrimeGenerator().Generate(n);
+
+            timer.Stop();
+
+            #region Verbose Output Options
+            if(outputPrimesUnordered)
+            {
+                Parallel.ForEach(primes, (prime) =>
+                {
+                    Console.WriteLine(prime);
+                });
+            }
+
+            if(outputPrimesOrdered)
+            {
+                foreach (var prime in primes)
+                {
+                    Console.WriteLine(prime);
+                }
+            }
+            #endregion
+
+            Console.WriteLine(string.Format("{0}:\tFinished generating primes...", timer.Elapsed.ToString()));
+            Console.WriteLine(string.Format("{0}:\t{1}th prime is {2}...", timer.Elapsed.ToString(), n, primes.Last()));
+            Console.ReadKey();
+        }
+    }
+}
